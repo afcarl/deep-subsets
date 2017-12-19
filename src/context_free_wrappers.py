de04f71b37a67c58f134a038719ffb17baff4734
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-12-20 01:08:32 +0530

    Add code for context free encoder in the context of sets

---
+++ src/context_free_wrappers.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ContextFreeEncoder(nn.Module):
+    def __init__(self, element_encoder, element_dims='1d'):
+        """
+        Applies a module on each element of the set independently.
+
+        WARNING: only tested with convolution-like encoders
+        :param element_encoder: the encoder to apply to each element of the set
+        :param element_dims: the type of element in the set (can be 1d or 2d)
+        :returns Variable: flattened extracted features
+        Example:
+        If your set contains 2D images and you 
+        want to use a Conv2D to extract features
+        ```
+        element_encoder = nn.Conv2D(3, 1, (2,2))
+        set_encoder = ContextFreeEncoder(element_encoder, '2d')
+        
+        # inputs:
+        # batch size x number of elements in set x channels x height x width
+        
+    
+        # outputs:
+        # batch_size x number of elements in set x -1
+        ```
+        """
+        super().__init__()
+        assert element_dims in ['1d', '2d']
+        self.element_encoder = element_encoder
+        self.element_dims = element_dims
+
+    def reshape_input(self, input):
+        """
+        Reshapes the inputs so that each element of the set
+        is represented as an element in the batch
+        """
+        if self.element_dims == '1d':
+            batch_size, set_size, channels, L = input.size()
+            reshaped = input.view(batch_size * set_size, channels, L)
+            return reshaped, (batch_size, set_size)
+        elif self.element_dims == '2d':
+            batch_size, set_size, channels, H, W = input.size()
+            reshaped = input.view(batch_size * set_size, channels, H, W)
+            return reshaped, (batch_size, set_size)
+        else:
+            raise ValueError('Unknown input size.')
+
+    def reshape_output(self, output, batch_size, set_size):
+        """
+        Reshapes the output so that the elements from the batch
+        are placed as elements of their respective sets.
+        """
+
+        output_sizes = output.size()
+        # print('output_sizes:',output_sizes)
+        reshaped = output.view(batch_size, set_size, *output_sizes[1:])
+        return reshaped
+
+    def forward(self, x):
+        x, sizes = self.reshape_input(x)
+        x = self.element_encoder(x)
+        # print('sizes:',sizes)
+        return self.reshape_output(x, *sizes)
+
+
---
+++ tests/test_context_free_encoder.py
@@ -0,0 +1,61 @@
+from src.context_free_wrappers import ContextFreeEncoder
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+class Simple2DConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(500, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 500)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+
+def test_1d_inputs():
+    # 2 sets
+    # 3 elements per set
+    # (1, 10) features per element per set
+    x = torch.ones(2, 3, 1, 10)
+    x[:, 1] = x[:, 1] + 1
+    x[1, 2] = x[1, 2] + 1
+
+    cfe = ContextFreeEncoder(nn.Conv1d(1, 2, 2), '1d')
+    y = cfe(Variable(x))
+    assert tuple(y.size()) == (2, 3, 2, 9)
+    y = y.view(2, 3, 2*9).data.numpy()
+    assert np.allclose(y[0, 1], y[1, 1])
+    assert np.allclose(y[0, 0], y[0, 2])
+    assert np.allclose(y[0, 1], y[1, 2])
+
+def test_2d_inputs():
+    # 2 sets
+    # 3 images per set
+    # 3 channels per image
+    # 32x32 is image size
+    pix = torch.ones(2, 3, 3, 32, 32)
+    pix[0][0].add_(1)
+    pix[0][2].add_(1)
+    pix[1][2].add_(1)
+
+    cfe = ContextFreeEncoder(Simple2DConvNet(), '2d')
+    cfe.train(False) # stop stochastic dropouts
+    y = cfe(Variable(pix))
+    # since this is a flattened extractor we should get:
+    assert tuple(y.size()) == (2, 3, 10)
+    y = y.data.numpy()
+    assert np.allclose(y[0][0], y[0][2])
+    assert np.allclose(y[0][1], y[1][1])
+    assert np.allclose(y[0][0], y[1][2])
