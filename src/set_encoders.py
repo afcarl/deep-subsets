eb2674d2a9b6bc670b903a8a0879aa62b49d9d45
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-12-20 13:18:27 +0530

    Add tests for parameter updating and equivariance

---
+++ src/set_encoders.py
@@ -0,0 +1,31 @@
+# encode elements of a set
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.autograd import Variable
+from context_free_wrappers import ContextFreeEncoder
+
+class ContextBasedLinear(nn.Module):
+    def __init__(self, nonlinearity=None):
+        super().__init__()
+        self.mu = Parameter(torch.Tensor(1))
+        self.gamma = Parameter(torch.Tensor(1))
+        self.nonlinearity = nonlinearity()
+        self.reset_weights()
+
+    def reset_weights(self):
+        self.mu.data.uniform_(-0.1, 0.1)
+        self.gamma.data.uniform_(-0.1, 0.1)
+
+    def forward(self, x):
+        elements_per_set = x.size(1)
+        ONE = Variable(torch.ones(elements_per_set), requires_grad=False)
+        I = Variable(torch.eye(elements_per_set), requires_grad=False)
+        weights = self.mu * I
+        weights += self.gamma * torch.ger(ONE, ONE)
+        x = torch.matmul(weights, x)
+
+        if self.nonlinearity:
+            x = self.nonlinearity(x)
+        return x
---
+++ tests/test_context_based_encoder.py
@@ -0,0 +1,79 @@
+from src.set_encoders import ContextBasedLinear
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+
+cbl = ContextBasedLinear(nn.ReLU)
+
+def test_equivariance():
+
+    # create two data batches
+    data1 = torch.ones(2, 3, 5)
+    data1[0, 1].add_(1)
+    data1[0, 2].add_(1)
+    data1[1, 2].add_(1)
+
+    data2 = torch.ones(*data1.size())
+    data2.copy_(data1)
+    # permute the first set:
+    data2[0, 0] = 2
+    data2[0, 1] = 1
+    data2[0, 2] = 2
+    
+
+    result1 = cbl(Variable(data1))
+    result2 = cbl(Variable(data2))
+
+    # check sizes
+    assert tuple(result2.size()) == tuple(result1.size())
+    assert tuple(result2.size()) == tuple(data1.size())
+
+    result1 = result1.data.numpy()
+    result2 = result2.data.numpy()
+
+    # second set doesnt change
+    assert np.allclose(result1[1],result2[1])
+
+    assert np.allclose(result1[0][0], result2[0][1])
+    assert np.allclose(result1[0][1], result2[0][2])
+
+def test_parameter_updating():
+    data1 = torch.ones(2, 3, 5)
+    data1[0, 1].add_(1)
+    data1[0, 2].add_(1)
+    data1[1, 2].add_(1)
+    data1 = Variable(data1)
+    target = Variable(torch.zeros(2))
+
+    # store initial parameters
+    gamma0 = cbl.gamma.data.numpy()[0]
+    mu0 = cbl.mu.data.numpy()[0]
+
+
+    class Loss(nn.Module):
+        def forward(self, input, target):
+            a = torch.sum(torch.sum(input, dim=1), dim=1)
+            return torch.sum((a - target)**2)
+
+    # prepare for training:
+    criterion = Loss()
+    sgd = torch.optim.SGD(cbl.parameters(), lr=0.01)
+
+    for i in range(100):
+        output = cbl(data1)
+        sgd.zero_grad()
+        loss = criterion(output, target)
+        loss.backward()
+        sgd.step()
+
+    mu100 = cbl.gamma.data.numpy()[0]
+    gamma100 = cbl.mu.data.numpy()[0]
+
+    # make sure that the parameters are updating:
+    assert mu100 != mu0
+    assert gamma100 != gamma0
+
