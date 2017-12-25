94d68ff7bf6cf0068f7ec06ea11ec2d7c71771e7
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-12-25 12:38:35 +0530

    Update MNIST extractor for other frameworks

--- src/networks/mnist.py
+++ src/networks/mnist.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from src.set_encoders import ContextBasedMultiChannelLinear, ContextFreeEncoder
+from src.set_encoders import ContextBasedLinear, ContextBasedMultiChannelLinear, ContextFreeEncoder
 from src.set_decoders import LinearSumSet, SimpleSubset
 from src.util_layers import FlattenElements
 
@@ -20,8 +20,6 @@ def get_MNIST_extractor():
                          ElementFlatten(),
                          nn.Linear(4608, 128),
                          nn.ReLU())
-        
-        
 
 class Set2RealNet(nn.Module):
     """
@@ -32,12 +30,7 @@ class Set2RealNet(nn.Module):
         super().__init__()
 
         # per element encoder is a conv neural network
-        cfe = nn.Sequential(nn.Conv2d(1, 1, (3, 3)),
-                            nn.MaxPool2d(4),
-                            nn.ReLU(),
-                            nn.Conv2d(1, 1, (3, 3)),
-                            nn.MaxPool2d(2),
-                            nn.ReLU())
+        cfe = get_MNIST_extractor()
         
         self.cfe = ContextFreeEncoder(cfe, '2d')
         self.flatten = FlattenElements()
@@ -49,9 +42,9 @@ class Set2RealNet(nn.Module):
             self.encode_set = False
 
         self.lss = LinearSumSet()
-        self.lineartoh = nn.Linear(4, 4)
+        self.lineartoh = nn.Linear(128, 64)
         self.relu = nn.ReLU()
-        self.linearout = nn.Linear(4, 1)
+        self.linearout = nn.Linear(64, 1)
         
     def forward(self, x):
         x = self.cfe(x) # encode individual images
@@ -78,19 +71,14 @@ class Seq2RealNet(nn.Module):
         super().__init__()
         
         # per element encoder is a conv neural network
-        cfe = nn.Sequential(nn.Conv2d(1, 1, (3, 3)),
-                            nn.MaxPool2d(4),
-                            nn.ReLU(),
-                            nn.Conv2d(1, 1, (3, 3)),
-                            nn.MaxPool2d(2),
-                            nn.ReLU())
+        cfe = get_MNIST_extractor()
 
         self.cfe = ContextFreeEncoder(cfe, '2d')
         self.flatten = FlattenElements()
 
-        self.lstm = nn.LSTM(4, 4, 1)
+        self.lstm = nn.LSTM(128, 64, 1)
         self.relu = nn.ReLU()
-        self.linearout = nn.Linear(4, 1)
+        self.linearout = nn.Linear(64, 1)
 
     def forward(self, x):
         x = self.cfe(x)
