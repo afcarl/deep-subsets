a053842938ad299a5fec71e199f6a0f6a7495169
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-12-28 08:11:07 +0530

    add set accuracy metric

---
+++ src/metrics.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+def set_accuracy(y_true, y_pred):
+    batch_size, set_size = y_true.size()
+    y_pred = nn.Sigmoid()(y_pred)
+    acc = torch.sum(torch.eq(y_pred, y_true).sum(dim=1) == set_size).float()/batch_size
+    return acc
