9e1eb41ec56678f8730d7ee3cf3f55eb94905d8c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-12-29 07:13:17 +0530

    Add random sampler for comparison

--- src/metrics.py
+++ src/metrics.py
@@ -6,3 +6,6 @@ def set_accuracy(y_true, y_pred):
     y_pred = nn.Sigmoid()(y_pred)
     acc = torch.sum(torch.eq(y_pred, y_true).sum(dim=1) == set_size).float()/batch_size
     return acc
+
+def random_samples(batch_size, set_size):
+    return torch.rand(batch_size, set_size) > 0.5
