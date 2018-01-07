ca6e1fecd0515d9007abad87768e59863fbfdcce
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-01-07 20:15:15 +0530

    Add sanity check dataset

--- src/datatools/__init__.py
+++ src/datatools/__init__.py
@@ -1,4 +1,4 @@
 from .set2real_data import MNISTSets
 from .set2subset_data import MNISTSubsets
 from .subsetsum_data import SubsetSum
-from .numbers_data import NumbersDataset
+from .numbers_data import NumbersDataset, IntergersLargerThanAverage
