Add script for the experiment itself, fix calculation of accuracy

src/metrics.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 def set_accuracy(y_true, y_pred):
     batch_size, set_size = y_true.size()
-    y_pred = nn.Sigmoid()(y_pred)
+    y_pred = (nn.Sigmoid()(y_pred) > 0.5).float()
     acc = torch.sum(torch.eq(y_pred, y_true).sum(dim=1) == set_size).float()/batch_size
     return acc
 