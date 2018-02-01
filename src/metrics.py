27e180f3861056d8a47e6a37d70d72679bb769db
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-02-01 13:11:10 -0500

    set_acc now returns per element accuracy as well

--- src/metrics.py
+++ src/metrics.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 def set_accuracy(y_true, y_pred):
     batch_size, set_size = y_true.size()
     y_pred = (nn.Sigmoid()(y_pred) > 0.5).float()
-    acc = torch.sum(torch.eq(y_pred, y_true).sum(dim=1) == set_size).float()/batch_size
-    return acc
+    correct_classifications = torch.eq(y_pred, y_true)
+    set_acc = torch.sum(correct_classifications.sum(dim=1) == set_size).float()/batch_size
+    # print(correct_classifications)
+    element_acc = torch.sum(correct_classifications).float()/(batch_size*set_size)
+    return set_acc, element_acc
 
 def random_samples(batch_size, set_size):
     return torch.rand(batch_size, set_size) > 0.5
