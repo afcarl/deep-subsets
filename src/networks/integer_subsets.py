104f11e6d6b1df8067189f66db3ad04fba1b4742
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-02-01 12:12:57 -0500

    Add basic integer subset network

--- src/networks/integer_subsets.py
+++ src/networks/integer_subsets.py
@@ -8,33 +8,34 @@ class IntegerSubsetNet(nn.Module):
     Used when the input is to be interpreted as a set of MNIST digits
     and the output is a subset of those elements as indicated by probabilities
     """    
-    def __init__(self, logprobs=True):
+    def __init__(self, logprobs=True, null_model=False):
         super().__init__()
 
         # per element encoder is a conv neural network
-        
-        cfe = nn.Linear(8, 8)
+        self.null_model = null_model
+        cfe = nn.Sequential(nn.Linear(8, 8),
+                            nn.ReLU(),
+                            nn.Linear(8, 8) if not null_model else nn.Linear(8, 1))
         self.cfe = ContextFreeEncoder(cfe, '1d')
-        self.cbe = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
-        self.cbe2 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
-        self.cbe3 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
-        self.cbe4 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
-        self.cbe5 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
-        self.cbe6 = nn.LSTM(8, 4, 1, bidirectional=True)
-        self.decoder = nn.LSTM(8, 1, 1)
+        if not self.null_model:
+            self.cbe = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
+            self.cbe2 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
+            self.cbe3 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
+            self.cbe4 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
+            self.cbe5 = ContextBasedMultiChannelLinear(8, 8, nonlinearity=nn.ReLU)
+            self.cbe6 = ContextBasedMultiChannelLinear(8, 1)
+
         self.logprobs = logprobs
 
     def forward(self, x):
         x = self.cfe(x)
-        x = self.cbe(x)
-        x = self.cbe2(x)
-        x = self.cbe3(x)
-        x = self.cbe4(x)
-        x = self.cbe5(x)
-        x = x.permute(1, 0, 2)
-        x = self.cbe6(x)[0]
-        x = self.decoder(x)
-        x = x[0].permute(1, 0, 2)
+        if not self.null_model:
+            x = self.cbe(x)
+            x = self.cbe2(x)
+            x = self.cbe3(x)
+            x = self.cbe4(x)
+            x = self.cbe5(x)
+            x = self.cbe6(x)
         if not self.logprobs:
             x = F.sigmoid(x)
         return x
