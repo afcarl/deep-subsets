9ca2825cfc84d0d4a7744990c6c8549398e7e6d7
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-02-01 20:52:12 -0500

    Streamline metrics and add test for it

--- src/metrics.py
+++ src/metrics.py
@@ -2,10 +2,14 @@ import torch
 import torch.nn as nn
 
 def set_accuracy(y_true, y_pred):
+    if len(tuple(y_true.size())) == 3:
+        y_true = y_true.squeeze(-1).float()
     batch_size, set_size = y_true.size()
     y_pred = (nn.Sigmoid()(y_pred) > 0.5).float()
-    acc = torch.sum(torch.eq(y_pred, y_true).sum(dim=1) == set_size).float()/batch_size
-    return acc
+    correct_classifications = (y_true == y_pred).float()
+    elem_acc = torch.mean(correct_classifications)
+    set_acc = (torch.sum(correct_classifications, 1) == set_size).float().mean()
+    return set_acc, elem_acc
 
 def random_samples(batch_size, set_size):
     return torch.rand(batch_size, set_size) > 0.5
