54c5d62b83d0df06154f0fd40d709485f65568c3
zafarali <zafarali.ahmed@gmail.com>
2018-02-11 10:40:35 -0500

    Do size checking in set_accuracy function

--- src/metrics.py
+++ src/metrics.py
@@ -2,8 +2,17 @@ import torch
 import torch.nn as nn
 
 def set_accuracy(y_true, y_pred):
+    """
+    Calculates the accuracy of selection based on
+    ground truth selection idx
+    :param y_true: Ground Truth Selection IDX
+    :param y_pred: Predicted Selection IDX
+    :return:
+    """
     if len(tuple(y_true.size())) == 3:
         y_true = y_true.squeeze(-1).float()
+    if len(tuple(y_pred.size())) == 3:
+        y_pred = y_pred.squeeze(-1).float()
     batch_size, set_size = y_true.size()
     y_pred = (nn.Sigmoid()(y_pred) > 0.5).float()
     correct_classifications = (y_true == y_pred).float()
